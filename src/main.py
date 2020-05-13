f0fd6b45fdd59aa0a25c277afcbc5a27185c9ee5
Abhey <abhey.pkl@gmail.com>
2020-05-13 12:03:35 +0530

    Added word segmentation code. Output in JSON

--- src/main.py
+++ src/main.py
@@ -2,12 +2,15 @@ from __future__ import division
 from __future__ import print_function
 
 import sys
+import os
+import json
 import argparse
 import cv2
 import editdistance
 from DataLoader import DataLoader, Batch
 from Model import Model, DecoderType
 from SamplePreprocessor import preprocess
+from WordSegmentation import wordSegmentation, prepareImg
 
 
 class FilePaths:
@@ -87,7 +90,6 @@ def validate(model, loader):
 	print('Character error rate: %f%%. Word accuracy: %f%%.' % (charErrorRate*100.0, wordAccuracy*100.0))
 	return charErrorRate
 
-
 def infer(model, fnImg):
 	"recognize text in image provided by file path"
 	img = preprocess(cv2.imread(fnImg, cv2.IMREAD_GRAYSCALE), Model.imgSize)
@@ -95,7 +97,61 @@ def infer(model, fnImg):
 	(recognized, probability) = model.inferBatch(batch, True)
 	print('Recognized:', '"' + recognized[0] + '"')
 	print('Probability:', probability[0])
+	return (recognized[0], probability[0])
 
+
+def LineData(decoderType,dump):
+	"""reads images from data/ and outputs the word-segmentation to out/"""
+
+	# read input images from 'in' directory
+	imgFiles = os.listdir('../data/lines/')
+	if not os.path.exists('../data/out/'):
+		os.mkdir('../data/out')
+	print(open(FilePaths.fnAccuracy).read())
+	out_dict={}
+	model = Model(open(FilePaths.fnCharList).read(), decoderType, mustRestore=True, dump=dump)
+
+	for (i,f) in enumerate(imgFiles):
+		#print('Segmenting words of sample %s'%f)
+		file_compo=f.split('.')
+		f=file_compo[0]
+		extension=file_compo[1]
+		out_dict[f] = []
+		
+		# read image, prepare it by resizing it to fixed height and converting it to grayscale
+		img = prepareImg(cv2.imread('../data/lines/%s'%f), 50)
+		
+		# execute segmentation with given parameters
+		# -kernelSize: size of filter kernel (odd integer)
+		# -sigma: standard deviation of Gaussian function used for filter kernel
+		# -theta: approximated width/height ratio of words, filter function is distorted by this factor
+		# - minArea: ignore word candidates smaller than specified area
+		res = wordSegmentation(img, kernelSize=25, sigma=11, theta=7, minArea=100)
+		
+		# write output to 'out/inputFileName' directory
+		if not os.path.exists('../data/out/%s'%f):
+			os.mkdir('../data/out/%s'%f)
+		
+		# iterate over all segmented words
+		#print('Segmented into %d words'%len(res))
+		for (j, w) in enumerate(res):
+			(wordBox, wordImg) = w
+			(x, y, w, h) = wordBox
+			cv2.imwrite('../data/out/%s/%d.png'%(f, j), wordImg) # save word
+			FilePaths.fnInfer = '../data/out/%s/%d.png'%(f,j)
+			result, prob = infer(model, FilePaths.fnInfer)
+			#updating output dictionary
+			out_dict[f].append(result)
+			#deleting intermediate file
+			os.remove('../data/out/%s/%d.png'%(f, j))
+			cv2.rectangle(img,(x,y),(x+w,y+h),0,1) # draw bounding box in summary image
+		
+		# output summary image with bounding boxes around words
+		cv2.imwrite('../data/out/%s/summary.png'%f, img)
+		#generating json output
+		json_object=json.dumps(out_dict, indent=4)
+		with open('../data/out/output.json', "w") as outfile:
+			outfile.write(json_object)
 
 def main():
 	"main function"
@@ -106,6 +162,7 @@ def main():
 	parser.add_argument('--beamsearch', help='use beam search instead of best path decoding', action='store_true')
 	parser.add_argument('--wordbeamsearch', help='use word beam search instead of best path decoding', action='store_true')
 	parser.add_argument('--dump', help='dump output of NN to CSV file(s)', action='store_true')
+	parser.add_argument('--line', help='the image is of a line', action='store_true')
 
 	args = parser.parse_args()
 
@@ -135,12 +192,14 @@ def main():
 			validate(model, loader)
 
 	# infer text on test image
+	elif args.line:
+		LineData(decoderType,args.dump)
+
 	else:
 		print(open(FilePaths.fnAccuracy).read())
 		model = Model(open(FilePaths.fnCharList).read(), decoderType, mustRestore=True, dump=args.dump)
-		infer(model, FilePaths.fnInfer)
+		_, _=infer(model, FilePaths.fnInfer)
 
 
 if __name__ == '__main__':
 	main()
-
